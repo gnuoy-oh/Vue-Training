34099ad8bebe6f2995bd3fcd961f0e5c04c15207
gnuoy-oh <oh.my00418@gmail.com>
2021-05-31 14:34:02 +0900

    Resolved: 사용자 프로필 컴포넌트 스타일링 및 코드 정리

--- vue-news/src/components/UserProfile.vue
+++ vue-news/src/components/UserProfile.vue
@@ -4,27 +4,32 @@
     <div>
       <i class="fas fa-user"></i>
     </div>
-    <div class="user-description">
-      <p>name: {{ userInfo.id }}</p>
-      <p>karma: {{ userInfo.karma }}</p>
-      <p>created: {{ userInfo.created }}</p>
-      <!-- <router-link :to="`/user/${fetchedItem.user}`">
-        {{ fetchedItem.user }}
-      </router-link>
-      <div class="time">
-        {{ fetchedItem.time_ago }}
-      </div> -->
+    <!-- 질문 상세 정보-->
+    <div class="user-container">
+      <div class="user-description">
+        <slot name="username">
+          <!-- 상위 컴포넌트에서 정의할 내용 -->
+          <!-- 해당 내부가 페이지 별로(UserView->id, ItemView->user) 접근하는 속성이 다르기 때문에 slot으로 작업한다. -->
+        </slot>
+        <div class="time">
+          <slot name="time">
+            <!-- 상위 컴포넌트에서 정의할 내용 -->
+          </slot>
+          <slot name="karma">
+            <!-- 상위 컴포넌트에서 정의할 내용 -->
+          </slot>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  computed: {
-    userInfo() {
-      return this.$store.state.user;
-    }
+  props: {
+    info: Object
   },
+  computed: {},
   created() {}
 };
 </script>
--- vue-news/src/views/UserView.vue
+++ vue-news/src/views/UserView.vue
@@ -1,21 +1,27 @@
 <template>
   <div class="wrap">
-    <userProfile></userProfile>
-    <p>name: {{ userInfo.id }}</p>
-    <p>karma: {{ userInfo.karma }}</p>
-    <p>created: {{ userInfo.created }}</p>
+    <UserProfile :info="userInfo">
+      <div slot="username">{{ userInfo.id }}</div>
+      <span slot="time">{{ `Joined ${userInfo.created}` }}, </span>
+      <span slot="karma">{{ userInfo.karma }}</span>
+    </UserProfile>
   </div>
 </template>
 
 <script>
-import userProfile from "../components/UserProfile.vue";
+import UserProfile from "../components/UserProfile.vue";
 export default {
   components: {
-    userProfile
+    UserProfile
+  },
+
+  computed: {
+    userInfo() {
+      return this.$store.state.user;
+    }
   },
 
   created() {
-    console.log(this.$route);
     const userName = this.$route.params.id;
     this.$store.dispatch("FETCH_USER", userName);
   }
